6906204413ab6796a1587ce8354bc6ced0bf3b88
StupPlayer <gabrielh.oliveira222@gmail.com>
2021-01-09 13:11:47 -0300

    Fix rendering when cooldown is active

---
+++ src/main/java/me/steven/pixelpets/mixin/MixinHeldItemRenderer.java
@@ -0,0 +1,23 @@
+package me.steven.pixelpets.mixin;
+
+import me.steven.pixelpets.PixelPetsMod;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.render.item.HeldItemRenderer;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Environment(EnvType.CLIENT)
+@Mixin(HeldItemRenderer.class)
+public class MixinHeldItemRenderer {
+    @Redirect(method = "updateHeldItems", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;areEqual(Lnet/minecraft/item/ItemStack;Lnet/minecraft/item/ItemStack;)Z"))
+    private boolean areStacksEqual(ItemStack original, ItemStack updated) {
+        if (updated.getItem().equals(original.getItem()) && (updated.getItem().equals(PixelPetsMod.PET_ITEM) || updated.getItem().equals(PixelPetsMod.EGG_ITEM) )) {
+            return true;
+        }
+
+        return ItemStack.areEqual(original, updated);
+    }
+}
